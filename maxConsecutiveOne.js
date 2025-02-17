5e354fda57b2f21c6d4c4468469cdf2ab76333af
S-a-k-s-h-i <sakshipokhria1997@gmail.com>
2025-02-17 18:10:07 +0530

    added comment

--- maxConsecutiveOne.js
+++ maxConsecutiveOne.js
@@ -12,6 +12,10 @@ var findMaxConsecutiveOnes = function(nums) {
             count1=0
         }
     }
+    //this condition added for case where the last digit is 1 so then
+    //we have not come under else condition and not yet stored the maxCons1
     maxCons1=Math.max(maxCons1,count1);
     return maxCons1;
 };
+
+console.log(findMaxConsecutiveOnes([1,1,0,1,1,1]))
